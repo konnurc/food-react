b5b4d6940d1799da152d2b123607cefe43f91f1c
konnurc7 <ckonnur@desototechnologies.com>
2026-04-16 11:38:39 +0530

    episode -1

---
+++ App.js
@@ -0,0 +1,13 @@
+var parent = React.createElement("div", {id: "parent"} , [
+             React.createElement("div", {id: "child1"} , [
+                React.createElement("h1", {id: "heading-1"} , "Child1 Heading -1"),
+                React.createElement("h2", {id: "heading-1"} , "Child1 Heading -2")
+            ]),
+            React.createElement("div", {id: "child2"} , [
+                React.createElement("h1", {id: "heading-1"} , "Child2 Heading -1"),
+                React.createElement("h2", {id: "heading-1"} , "Child2    Heading -2")
+            ])
+        ]);
+        console.log(parent);
+        var root = ReactDOM.createRoot(document.getElementById('root'));
+        root.render(parent)
