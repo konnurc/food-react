865fa8d1b938d8f23c6ca1d559a9cb59495302a9
konnurc7 <ckonnur@desototechnologies.com>
2026-04-24 12:32:37 +0530

    episode-2

--- App.js
+++ App.js
@@ -1,16 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-var parent = React.createElement("div", {id: "parent"} , [
-             React.createElement("div", {id: "child1"} , [
-                React.createElement("h1", {id: "heading-1"} , "Child1 Heading -1"),
-                React.createElement("h2", {id: "heading-1"} , "Child1 Heading -2")
-            ]),
-            React.createElement("div", {id: "child2"} , [
-                React.createElement("h1", {id: "heading-1"} , "Child2 Heading -1"),
-                React.createElement("h2", {id: "heading-1"} , "Child2    Heading -2")
-            ])
-        ]);
-        console.log(parent);
-        var root = ReactDOM.createRoot(document.getElementById('root'));
-        root.render(parent)
+var rupees = 1000;
+var title = React.createElement("span", {}, "minicart Title");
+var titleJSX = <span>Minicart KSX Title</span>;
+
+const HeaderComponent = () => {
+    return (
+        <div className="container">
+             {rupees},
+        {title},
+        {titleJSX}
+
+            Header Component
+        </div>
+    );
+}
+
+var root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<HeaderComponent />)
