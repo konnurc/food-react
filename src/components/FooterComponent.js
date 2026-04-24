232c5a80f76d1171f660db84a76069e620975b78
konnurc7 <ckonnur@desototechnologies.com>
2026-04-24 17:46:39 +0530

    episode-3

---
+++ src/components/FooterComponent.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div className="footer-container">
+      <div className='container'>
+          <div className="footer-copy-text">Food applications copy rights are reserved</div>
+          <div className="footer-links-and-social">
+
+            <div className="footer-social-link">
+              <ul>
+                <li><a href="#">Facebook</a></li>
+                <li><a href="#">Twitter</a></li>
+                <li><a href="#">YouTube</a></li>
+                <li><a href="#">Instagram</a></li>
+              </ul>
+            </div>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default FooterComponent;
